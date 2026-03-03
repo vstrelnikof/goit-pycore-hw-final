4f2a52293e11f375b808e111f8044e2192e3b5ca
Leshiy <v.strelnikof@gmail.com>
2026-03-03 03:21:45 +0200

    Enhance ConfigProvider to support command line arguments for configuration; refactor load method to merge file and argument settings

--- providers/config_provider.py
+++ providers/config_provider.py
@@ -1,24 +1,46 @@
 import logging
 import yaml
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from models.app_config import AppConfig
 
 class ConfigProvider:
-    """Провайдер конфігурації застосунку через файл"""
+    """Провайдер конфігурації застосунку через файл та через аргументи командного рядка"""
 
     @staticmethod
     def load() -> AppConfig:
-        """Фабричний метод який читає конфігурацію із файлу"""
+        """Фабричний метод який читає конфігурацію"""
         config_path = Path("config.yaml")
-        default_app_cfg = AppConfig()
+        default_config = AppConfig()
         if not config_path.exists():
             logging.warning("Config file not found: %s", config_path)
-            return default_app_cfg
+            return default_config
         try:
+            args_config = ConfigProvider.__parse_app_args()
             with open(config_path, "r", encoding="utf-8") as f:
-                config = yaml.safe_load(f) or {}
-                return AppConfig(**config.get("app", {}))
+                file_config = yaml.safe_load(f) or {}
+                merged_config = ConfigProvider.__merge_configs(
+                    file_config, args_config, default_config)
+                return AppConfig(**merged_config)
         except Exception as e:
             logging.error("Cannot create application config")
             logging.exception(e)
-            return default_app_cfg
+            return default_config
+
+    @staticmethod
+    def __parse_app_args() -> Namespace:
+        """Метод парсить налаштування із аргументів командного рядка"""
+        parser = ArgumentParser(description="Personal assistant configuration")
+        parser.add_argument("--theme", type=str, help="Personal assistant theme")
+        parser.add_argument("--log-level", type=int, help="Logging level")
+        args = parser.parse_args()
+        return args
+    
+    @staticmethod
+    def __merge_configs(file_config: dict, args_config: Namespace, default: AppConfig) -> dict:
+        """Метод мержить налаштування із конфіг-файлу та із аргументів командного рядка"""
+        merged = dict(file_config.get("app", default.__dict__))
+        for k, v in vars(args_config).items():
+            if v is not None:
+                merged[k] = v
+        return merged
