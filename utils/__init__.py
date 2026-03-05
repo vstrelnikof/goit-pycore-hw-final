2aa814a940f08853d0677b0e26bb4b00a2efdfc8
Leshiy <v.strelnikof@gmail.com>
2026-03-05 13:48:12 +0200

    Refactor exports in utils module to prevent circular imports; update __all__ to include only lightweight utilities and add documentation for import guidelines.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,9 @@
-from utils.state import AppState
+"""Щоб уникнути циклічних імпортів, тут експортуються лише «легкі» утиліти.
+Більш високорівневі об'єкти (наприклад, AppState) імпортуй безпосередньо
+з їхніх модулів: ``from utils.state import AppState``.
+"""
+
 from utils.validator import Validator
-from utils.fake_data import create_fakes
 from utils.wrapped_func_formatter import WrappedFuncNameFormatter
 
-__all__ = ["AppState", "Validator", "create_fakes", "WrappedFuncNameFormatter"]
+__all__ = ["Validator", "WrappedFuncNameFormatter"]
